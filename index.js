8282ed0a3060e20577b71cefbd14cce59014937e
Vivek gupta <me.vivekgupta9@gmail.com>
2023-08-09 16:35:26 +0530

    task2_sharpener

---
+++ index.js
@@ -0,0 +1,24 @@
+// // console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.doctype);
+
+// console.log(document.all);
+
+// var headerTitle = document.getElementById("header-title");
+// let header = document.getElementById("main-header");
+
+// headerTitle.innerText = "Hello";
+// console.log(headerTitle.textContent);
+// headerTitle.innerHTML = "<h3>Hello</h3>";
+// console.log(headerTitle);
+
+// header.style.borderBottom = "solid 5px black";
+
+var items = document.getElementsByClassName("list-group-item");
+console.log(items);
+console.log(items[2]);
+
+// items[2].style.fontWeight = "bold";
+items[1].style.backgroundColor = "pink";
+// items[2].style.backgroundColor = "yellow";
