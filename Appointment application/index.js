718cb490c5b8a7be7349339f3cf4a860cc0bfbac
Vivek gupta <me.vivekgupta9@gmail.com>
2023-08-11 16:25:36 +0530

    Delete data from Local Storage

--- Appointment application/index.js
+++ Appointment application/index.js
@@ -1,46 +1,38 @@
-document
-  .getElementById("userForm")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
+document.getElementById("userForm").addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    let name = document.getElementById("userName").value;
-    let email = document.getElementById("userEmail").value;
-    let phone = document.getElementById("userPhone").value;
+  // Get user details from form
+  let name = document.getElementById("userName").value;
+  let email = document.getElementById("userEmail").value;
+  let phone = document.getElementById("userPhone").value;
 
-    let newUser = {
-      name: name,
-      email: email,
-      phone: phone,
-    };
+  // Create user object
+  let user = {
+    name: name,
+    email: email,
+    phone: phone,
+  };
 
-    // Retrieve existing users from localStorage
-    let existingUsers = JSON.parse(localStorage.getItem("users") || "[]");
+  // Save to localStorage
+  localStorage.setItem(`${email}`, JSON.stringify(user));
 
-    // Add the new user to the existing users array
-    existingUsers.push(newUser);
-
-    // Save the updated array of users to localStorage
-    localStorage.setItem("users", JSON.stringify(existingUsers));
-
-    displayUsers();
-  });
-
-function displayUsers() {
+  // Display the
   let userList = document.getElementById("userList");
-  userList.innerHTML = "";
+  let li = document.createElement("li");
+  li.innerHTML = `<strong>Name:</strong> ${user.name}, 
+        <strong>Email:</strong> ${user.email}, 
+        <strong>Phone:</strong> ${user.phone}
+  `;
 
-  // Retrieve and display users from localStorage
-  let savedUsers = JSON.parse(localStorage.getItem("users") || "[]");
-  savedUsers.forEach((user) => {
-    let userItem = document.createElement("li");
-    userItem.innerHTML = `
-            <strong>Name:</strong> ${user.name}, 
-            <strong>Email:</strong> ${user.email}, 
-            <strong>Phone:</strong> ${user.phone}
-        `;
-    userList.appendChild(userItem);
+  let delButton = document.createElement("button");
+  delButton.className = "deleteButton";
+  delButton.appendChild(document.createTextNode("Delete"));
+  li.appendChild(delButton);
+  userList.appendChild(li);
+
+  //deleting
+  delButton.addEventListener("click", (e) => {
+    localStorage.removeItem(`${email}`);
+    userList.removeChild(li);
   });
-}
-
-// Display existing users on page load
-window.onload = displayUsers;
+});
